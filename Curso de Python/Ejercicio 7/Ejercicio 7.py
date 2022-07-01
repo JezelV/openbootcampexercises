c372bcbb56cbfc6fdf679ba4fd705c77a4006a85
Edgar Alanis <jezel88@gmail.com>
2022-07-01 18:37:46 -0500

    Solve Ejercicio 7

---
+++ Curso de Python/Ejercicio 7/Ejercicio 7.py
@@ -0,0 +1,8 @@
+edad = input("Por favor ingresa tu edad: ")
+
+if edad.isdigit():
+    edad = int(edad)
+    if edad >= 18:
+        print("Eres mayor de edad")
+    else:
+        print("Eres menor de edad")
